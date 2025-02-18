2e612816ff8ab413b7f8a64ab644f01d3c09c9e3
prasannakumarvallem <vpkr1570@gmail.com>
2025-02-18 11:19:26 +0530

    added chat feature and play on youtube

--- engine/command.py
+++ engine/command.py
@@ -1,14 +1,18 @@
 import pyttsx3
 import speech_recognition as sr
 import eel
+import time
 
 
 def speak(text):
+    text = str(text)
     engine = pyttsx3.init()
+    eel.DisplayMessage(text)
     engine.say(text)
+    eel.receiverText(text)
     engine.runAndWait()
 
-@eel.expose
+
 def takecommand():
 
     r = sr.Recognizer()
@@ -27,9 +31,33 @@ def takecommand():
         query = r.recognize_google(audio, language='en-in')
         print(f'User said: {query}')
         eel.DisplayMessage(query)
-        speak(query)
-        eel. ShowHood()
+        time.sleep(2)
+       
     except Exception as e:
         return ""
     
     return query.lower()
+
+@eel.expose
+def allCommands(message=1):
+
+    if message == 1:
+        query = takecommand()
+        print(query)
+        eel.senderText(query)
+    else:
+        query  = message
+        eel.senderText(query)
+    try:
+       
+        if "open" in query:
+            from engine.features import openCommand
+            openCommand(query)
+        elif "on youtube":
+            from engine.features import PlayYoutube
+            PlayYoutube(query)
+        else:
+            print("error or features need to add")
+    except:
+        print("error")
+    eel.ShowHood()
--- engine/features.py
+++ engine/features.py
@@ -1,8 +1,37 @@
 from playsound import playsound
 import eel
+from engine.command import speak
+from engine.config import ASSISTANT_NAME
+import os
+import pywhatkit as kit
+import re
 
 # playing assistant sound function
 @eel.expose
 def playAssistantSound():
-    music_dir = 'www\\assests\\audio\\start_sound.mp3'
+    music_dir = 'www\\assets\\audio\\start_sound.mp3'
     playsound(music_dir)
+
+def openCommand(query):
+    query = query.replace(ASSISTANT_NAME, "")
+    query = query.replace("open", "")
+    query.lower()
+
+    if query!="" :
+        speak("Opening "+query)
+        os.system('start '+query)
+    else:
+        speak("not found")
+
+def PlayYoutube(query):
+    search_term = extract_yt_term(query)
+    speak("Playing "+search_term+" on YouTube")
+    kit.playonyt(search_term)
+
+def extract_yt_term(command):
+    #define a regular expression pattern to capture the song name
+    pattern = r'play\s+(.*?)\s+on\s+youtube'
+    #Use re,search to find the match in the command
+    match = re.search(pattern, command, re.IGNORECASE)
+    #if a match is found,return the extracted song name;
+    return match.group(1) if match else None
